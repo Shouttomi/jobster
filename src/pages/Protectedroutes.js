c43f03db80654b686a39fde213b5349a5dcba3cf
Shouttomi <omiathak2002@gmail.com>
2023-06-22 00:39:40 +0530

    first commit

---
+++ src/pages/Protectedroutes.js
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router-dom";
+
+const Protectedroutes = ({children}) => {
+
+    console.log("protected")
+    const {user} = useSelector((store)=>store.user);
+
+  if(!user){
+    return <Navigate to='/landing'/>
+  }
+  return children;
+}
+export default Protectedroutes
